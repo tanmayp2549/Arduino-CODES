bcdf408f1699292309fd6059cf9d1482b82ac4a6
tanmayp2549 <pathaktanmay2549@gmail.com>
2026-04-10 15:57:13 +0530

    Add files via upload

---
+++ LEDlight.ino
@@ -0,0 +1,13 @@
+// This is my first IOT program
+#define my_led 7  //Global declaration
+
+
+void setup() {
+ pinMode(my_led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(my_led, HIGH);
+  // put your main code here, to run repeatedly:
+
+}
